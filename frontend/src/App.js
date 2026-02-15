b8fb6fcb3940a53bed3a07a8caf78e2ffcbf667c
THEEPIREDDY DHEERAJ REDDY <dheerajreddy0003@gmail.com>
2026-02-15 11:52:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,11 @@
-import EmployeeTable from './pages/EmployeeTable'; export default function App(){return <EmployeeTable/>;}
+import EmployeeTable from "./pages/EmployeeTable";
+import SalaryDisplay from "./components/SalaryDisplay";
+
+export default function App() {
+  return (
+    <div>
+      <EmployeeTable />
+      <SalaryDisplay />
+    </div>
+  );
+}
