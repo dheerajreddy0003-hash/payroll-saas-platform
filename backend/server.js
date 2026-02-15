75635a7132aca399abd86d047275d8723db64083
THEEPIREDDY DHEERAJ REDDY <dheerajreddy0003@gmail.com>
2026-02-15 11:45:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
-
 const express = require("express");
 const cors = require("cors");
+
 const employeeRoutes = require("./routes/employeeRoutes");
+const payrollRoutes = require("./routes/payrollRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// Employee APIs
 app.use("/api/employees", employeeRoutes);
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+// Payroll APIs
+app.use("/api/payroll", payrollRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
